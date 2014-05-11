3a75e29d53f9e08e55d9c61cf6d1ffe02ddf6297
Gary V. Vaughan <gary@gnu.org>
2014-05-11 16:45:32 +0700

    grammar: move all YAML patterns into YAML repository.
    * lib/zile/grammar/yaml-bundle.lua: Instead of verbatim pattern
    matching rules, move everything into the repository, and include
    from there.
    (unquoted_block): Allow nesting of blocks by also including all
    pattern matching rules again.
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

--- lib/zile/grammar/yaml-bundle.lua
+++ lib/zile/grammar/yaml-bundle.lua
@@ -6,50 +6,14 @@
   foldingStopMarker = [[^\s*$|^\s*\}|^\s*\]|^\s*\)]],
   keyEquivalent = '^~Y',
   repository = {
-    escaped_char = {
-      name = 'constant.character.escape.yaml',
-      match = [[\\.]],
-    },
-    unquoted_block = {
-      name = 'string.unquoted.block.yaml',
-      begin = [[^(\s*)(?:(-)|(?:(-\s*)?(\w+\s*(:))))(?:\s*(\||>))?]],
-      beginCaptures = {
-        [2] = { name = 'punctuation.definition.entry.yaml' },
-        [3] = { name = 'punctuation.definition.entry.yaml' },
-        [4] = { name = 'entity.name.tag.yaml' },
-        [5] = { name = 'punctuation.separator.key-value.yaml' },
-      },
-      ['end'] = [[^(?!^\1)|^(?=\1(-|\w+\s*:)|#)]],
-      patterns = {
-        { include = '#unquoted_block' },
-        { include = 'source.lua' },
-      },
-    },
-  },
-  patterns = {
-    { include = '#unquoted_block' },
-    {
-      name = 'constant.numeric.yaml',
-      match = [[(?:(?:(-\s*)?(\w+\s*(:)))|(-))\s*((0(x|X)[0-9a-fA-F]*)|(([0-9]+\.?[0-9]*)|(\.[0-9]+))((e|E)(\+|-)?[0-9]+)?)(L|l|UL|ul|u|U|F|f)?\s*$]],
+    comment = {
+      name = 'comment.line.number-sign.yaml',
+      match = [[(?<!\$)(#)(?!\{).*$\n?]],
       captures = {
-        [1] = { name = 'punctuation.definition.entry.yaml' },
-        [2] = { name = 'entity.name.tag.yaml' },
-        [3] = { name = 'punctuation.separator.key-value.yaml' },
-        [4] = { name = 'punctuation.definition.entry.yaml' },
+        [1] = { name = 'punctuation.definition.comment.yaml' },
       },
     },
-    {
-      name = 'string.unquoted.yaml',
-      match = [[(?:(?:(-\s*)?(\w+\s*(:)))|(-))\s*([A-Za-z0-9].*)\s*$]],
-      captures = {
-        [1] = { name = 'punctuation.definition.entry.yaml' },
-        [2] = { name = 'entity.name.tag.yaml' },
-        [3] = { name = 'punctuation.separator.key-value.yaml' },
-        [4] = { name = 'punctuation.definition.entry.yaml' },
-        [5] = { name = 'string.unquoted.yaml' },
-      },
-    },
-    {
+    date = {
       name = 'constant.other.date.yaml',
       match = [[(?:(?:(-\s*)?(\w+\s*(:)))|(-))\s*([0-9]{4}-[0-9]{2}-[0-9]{2})\s*$]],
       captures = {
@@ -59,24 +23,7 @@
         [4] = { name = 'punctuation.definition.entry.yaml' },
       },
     },
-    {
-      name = 'meta.tag.yaml',
-      match = [[(\w.*?)(:)\s*((\!\!)omap)?]],
-      captures = {
-        [1] = { name = 'entity.name.tag.yaml' },
-        [2] = { name = 'punctuation.separator.key-value.yaml' },
-        [3] = { name = 'keyword.other.omap.yaml' },
-        [4] = { name = 'punctuation.definition.keyword.yaml' },
-      },
-    },
-    {
-      name = 'variable.other.yaml',
-      match = [[(\&|\*)\w.*?$]],
-      captures = {
-        [1] = { name = 'punctuation.definition.variable.yaml' },
-      },
-    },
-    {
+    double_quote = {
       name = 'string.quoted.double.yaml',
       begin = [["]],
       beginCaptures = {
@@ -90,21 +37,15 @@
         { include = '#escaped_char' },
       },
     },
-    {
-      name = 'string.quoted.single.yaml',
-      begin = [[']],
-      beginCaptures = {
-        [0] = { name = 'punctuation.definition.string.begin.yaml' },
-      },
-      ['end'] = [[']],
-      endCaptures = {
-        [0] = { name = 'punctuation.definition.string.end.yaml' },
-      },
-      patterns = {
-        { include = '#escaped_char' },
-      },
+    escaped_char = {
+      name = 'constant.character.escape.yaml',
+      match = [[\\.]],
     },
-    {
+    hyphen = {
+      name = 'keyword.operator.symbol',
+      match = [[-]],
+    },
+    interpolated = {
       name = 'string.interpolated.yaml',
       begin = [[`]],
       beginCaptures = {
@@ -118,7 +59,15 @@
         { include = '#escaped_char' },
       },
     },
-    {
+    invalid_leading_tabs = {
+      name = 'invalid.leading-tabs.yaml',
+      match = [[^(\t+)]],
+    },
+    invalid_trailing_whitespace = {
+      name = 'invalid.deprecated.trailing-whitespace.yaml',
+      match = [[(\s+)$]],
+    },
+    merge_key = {
       name = 'keyword.operator.merge-key.yaml',
       match = [[(\\<\\<) = ((\\*).*)$]],
       captures = {
@@ -127,24 +76,101 @@
         [3] = { name = 'punctuation.definition.keyword.yaml' },
       },
     },
-    {
-      name = 'invalid.deprecated.trailing-whitespace.yaml',
-      match = [[(\s+)$]],
-    },
-    {
-      name = 'comment.line.number-sign.yaml',
-      match = [[(?<!\$)(#)(?!\{).*$\n?]],
+    numeric_constant = {
+      name = 'constant.numeric.yaml',
+      match = [[(?:(?:(-\s*)?(\w+\s*(:)))|(-))\s*((0(x|X)[0-9a-fA-F]*)|(([0-9]+\.?[0-9]*)|(\.[0-9]+))((e|E)(\+|-)?[0-9]+)?)(L|l|UL|ul|u|U|F|f)?\s*$]],
       captures = {
-        [1] = { name = 'punctuation.definition.comment.yaml' },
+        [1] = { name = 'punctuation.definition.entry.yaml' },
+        [2] = { name = 'entity.name.tag.yaml' },
+        [3] = { name = 'punctuation.separator.key-value.yaml' },
+        [4] = { name = 'punctuation.definition.entry.yaml' },
       },
     },
-    {
-      name = 'keyword.operator.symbol',
-      match = [[-]],
+    single_quote = {
+      name = 'string.quoted.single.yaml',
+      begin = [[']],
+      beginCaptures = {
+        [0] = { name = 'punctuation.definition.string.begin.yaml' },
+      },
+      ['end'] = [[']],
+      endCaptures = {
+        [0] = { name = 'punctuation.definition.string.end.yaml' },
+      },
+      patterns = {
+        { include = '#escaped_char' },
+      },
     },
-    {
-      name = 'invalid.leading-tabs.yaml',
-      match = [[^(\t+)]],
+    tag = {
+      name = 'meta.tag.yaml',
+      match = [[(\w.*?)(:)\s*((\!\!)omap)?]],
+      captures = {
+        [1] = { name = 'entity.name.tag.yaml' },
+        [2] = { name = 'punctuation.separator.key-value.yaml' },
+        [3] = { name = 'keyword.other.omap.yaml' },
+        [4] = { name = 'punctuation.definition.keyword.yaml' },
+      },
     },
+    unquoted_block = {
+      name = 'string.unquoted.block.yaml',
+      begin = [[^(\s*)(?:(-)|(?:(-\s*)?(\w+\s*(:))))(?:\s*(\||>))]],
+      beginCaptures = {
+        [2] = { name = 'punctuation.definition.entry.yaml' },
+        [3] = { name = 'punctuation.definition.entry.yaml' },
+        [4] = { name = 'entity.name.tag.yaml' },
+        [5] = { name = 'punctuation.separator.key-value.yaml' },
+      },
+      ['end'] = [[^(?!^\1)|^(?=\1(-|\w+\s*:)|#)]],
+      patterns = {
+        { include = '#unquoted_block' },
+        { include = '#numeric_constant' },
+        { include = '#unquoted_string' },
+        { include = '#date' },
+        { include = '#tag' },
+        { include = '#variable' },
+        { include = '#double_quote' },
+        { include = '#single_quote' },
+        { include = '#interpolated' },
+        { include = '#merge_key' },
+        { include = '#invalid_trailing_whitespace' },
+        { include = '#comment' },
+        { include = '#hyphen' },
+        { include = '#invalid_leading_tabs' },
+        { include = 'source.lua' },
+      },
+    },
+    unquoted_string = {
+      name = 'string.unquoted.yaml',
+      match = [[(?:(?:(-\s*)?(\w+\s*(:)))|(-))\s*([A-Za-z0-9].*)\s*$]],
+      captures = {
+        [1] = { name = 'punctuation.definition.entry.yaml' },
+        [2] = { name = 'entity.name.tag.yaml' },
+        [3] = { name = 'punctuation.separator.key-value.yaml' },
+        [4] = { name = 'punctuation.definition.entry.yaml' },
+        [5] = { name = 'string.unquoted.yaml' },
+      },
+    },
+    variable = {
+      name = 'variable.other.yaml',
+      match = [[(\&|\*)\w.*?$]],
+      captures = {
+        [1] = { name = 'punctuation.definition.variable.yaml' },
+      },
+    },
+  },
+  patterns = {
+    { include = '#unquoted_block' },
+    { include = '#numeric_constant' },
+    { include = '#unquoted_string' },
+    { include = '#date' },
+    { include = '#tag' },
+    { include = '#variable' },
+    { include = '#double_quote' },
+    { include = '#single_quote' },
+    { include = '#interpolated' },
+    { include = '#merge_key' },
+    { include = '#invalid_trailing_whitespace' },
+    { include = '#comment' },
+    { include = '#hyphen' },
+    { include = '#invalid_leading_tabs' },
   },
 }
