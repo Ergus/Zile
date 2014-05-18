750074405ff90008ef1fd57555ee2c1442d858aa
Gary V. Vaughan <gary@gnu.org>
2014-05-18 16:28:05 +0700

    Array: ensure cloning existing arrays works correctly.
    * lib/zile/specs/Array_spec.yaml (__call): Redo the specifications
    to ensure cloning an existing array copies elements correctly.
    Use a more `alien.array` like API for instantiating new arrays.
    (realloc): Add missing specifications.
    * lib/zile/Array.lua (_init): Implement new API to meet new
    specifications. Use lib.argcheck to check argument types.
    (copy, setzero): New private helper functions.
    (setlen): Rename from this...
    (realloc): ...to this, for consistency with alien.array APIs.
    Manage array.used field from here.  Use lib.argcheck.
    (push): Simplify. Use lib.argscheck.
    (pop, __index, __newindex): Simplify.
    (__tostring): New function. Return a string that will recreate the
    array if passed to Lua load().
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

--- lib/zile/Array.lua
+++ lib/zile/Array.lua
@@ -26,7 +26,7 @@
      > Array = require "zile.Array"
      > array = Array ("int", 0xdead, 0xbeef, 0xfeed)
      > =array[1], array[2], array[3], array[-1]
-     57005	48879	nil	65261
+     57005	48879	65261	65261
 
  All the indexes passed to methods use 1-based counting.
 
@@ -37,9 +37,29 @@
 local alien  = require "alien"
 local Object = require "std.object"
 
-local calloc = alien.array
+local lib    = require "zile.lib"
+
+local prototype = Object.prototype
+local typeof = type
+local calloc, memset = alien.array, alien.memset
+local argcheck, argscheck = lib.argcheck, lib.argscheck
 
 local element_chunk_size = 16
+
+
+--- Fast zeroing of a contiguous block of array elements.
+-- @tparam alien.array array an array
+-- @int from index of first element to zero out
+-- @int n number of elements to zero out
+-- @treturn alien.array array
+local function setzero (array, from, n)
+  if n > 0 then
+    local size = array.size
+    local b = (from - 1) * size + 1
+    memset (array.buffer:topointer (b), 0, n * size)
+  end
+  return array
+end
 
 
 local _functions = {
@@ -47,51 +67,63 @@ local _functions = {
   -- @function pop
   -- @return the right-most element
   pop = function (self)
-    if self.used > 0 then
+    argscheck ("pop", {"Array"}, {self})
+
+    local used = self.used
+    if used > 0 then
       local a = self.array
-      local elem = a[self.used]
-      self.used = self.used - 1
-      if self.used < a.length / 2 then
-        self:setlen (self.used)
-      end
+      local elem = a[used]
+      self:realloc (used - 1)
       return elem
     end
   end,
+
 
   --- Add elem as the new right-most element.
   -- @function push
   -- @param elem new element to be pushed
   -- @treturn Array the array
   push = function (self, elem)
-    if type (elem) ~= "number" then
-      return error ("bad argument #2 to 'push' (number expected, got " ..
-                    (elem and type (elem) or "no value") .. ")", 2)
-    end
-    local a = self.array
-    self.used = self.used + 1
-    if self.used > a.length then
-      self:setlen (self.used)
-    end
-    a[self.used] = elem
+    argscheck ("push", {"Array", "number"}, {self, elem})
+
+    local a, used = self.array, self.used + 1
+    self:realloc (used)
+    a[used] = elem
     return self
   end,
 
 
   --- Change the number of elements allocated to be at least `n`.
-  -- @function setlen
+  -- @function realloc
   -- @int n the number of elements required
   -- @treturn Array the array
-  setlen  = function (self, n)
-    local a = self.array
+  realloc = function (self, n)
+    argscheck ("realloc", {"Array", "number"}, {self, n})
+
+    local a, used = self.array, self.used
     if n > a.length or n < a.length / 2 then
       a:realloc (n + element_chunk_size)
     end
+
+    -- Zero padding for uninitialised elements.
+    setzero (a, used + 1, n - used)
+
+    self.used = n
     return self
   end,
 }
 
 
-local _type = type
+--- Copy an alien.array.
+-- @tparam alien.array array array to be copied
+-- @treturn alien.array a new array identical to `array`
+local function copy (array)
+  local a = calloc (array.type, array.length)
+  local n = array.size * array.length
+  alien.memmove (a.buffer:topointer (1), array.buffer:topointer (1), n)
+  return a
+end
+
 
 ------
 -- An efficient array of homogenous objects.
@@ -102,45 +134,80 @@ local Array = Object {
   _type = "Array",
 
 
+  -- Prototype initial values.
+  used  = 0,
+  array = calloc ("int", {0}),
+
+
   -- Module functions.
   _functions = _functions,
 
 
   --- Instantiate a newly cloned Array.
   -- @function __call
-  -- @string type the C type of each element
-  -- @param[opt] ... initial elements to store
+  -- @string[opt] type the C type of each element
+  -- @tparam[opt] int|table init initial size or list of initial elements
   -- @treturn Array a new Array object
-  _init = function (self, type, ...)
-    if _type (type) ~= "string" then
-      return error ("bad argument #1 to 'Array' (string expected, got " ..
-                    (type and  _type (type) or "no value") ..")", 2)
+  _init = function (self, type, init)
+    if init ~= nil then
+      -- When called with 2 arguments:
+      argcheck ("Array", 1, {"string"}, type)
+      argcheck ("Array", 2, {"number", "table"}, init)
+    elseif type ~= nil then
+      -- When called with 1 argument:
+      argcheck ("Array", 1, {"number", "string", "table"}, type)
     end
-    self.used  = select ("#", ...)
-    self.array = calloc (type, math.max (self.used, 1))
-    for i, v in ipairs {...} do
-      if _type (v) ~= "number" then
-        return error (string.format (
-          "bad argument #%d to 'Array' (number expected, got %s)", i + 2, _type (v)),
-          2
-        )
+
+    local parray, pused  = self.array, self.used
+    local plength, ptype = parray.length, parray.type
+
+    -- Non-string argument 1 is really an init argument.
+    if typeof (type) ~= "string" then type, init = nil, type end
+
+    -- New array type is copied from prototype if not specified.
+    if type == nil then type = ptype end
+
+    local a
+    if init == nil then
+      -- 1a. A fast clone of prototype's array memory.
+      if type == ptype then
+        a = copy (parray)
+
+      -- 1b. Size of elements changed, so we have to manually copy them
+      --     over where they are truncated or expanded as necessary.
+      else
+        a = calloc (type, plength)
+        for i = 1, plength do
+          a[i] = parray[i]
+        end
       end
-      self.array[i] = v
+
+    elseif typeof (init) == "number" then
+      -- 2a. Clone a number of elements from the prototype, padding with
+      --     zeros if we have more elements than the prototype.
+      if type == ptype then
+        a = copy (parray)
+        a:realloc (init) -- alien.array.realloc, not zile.Array.realloc!!
+
+      -- 2b. Same, but element-wise copying with a different sized type.
+      else
+        a = calloc (type, init)
+        for i = 1, math.min (init, plength) do
+          a[i] = parray[i]
+        end
+      end
+
+      setzero (a, pused + 1, init - pused)
+      self.used = init
+
+    elseif typeof (init) == "table" then
+      -- 3. With an initialisation table, ignore prototype elements.
+      a = calloc (type, init)
+      self.used = #init
     end
+
+    self.array = a
     return self
-  end,
-
-
-  --- Return a string representation of the contents of this Array.
-  -- @function __tostring
-  -- @treturn string string representation
-  __tostring = function (self)
-    local a = self.array
-    local t = {'"' .. a.type .. '"'}
-    for i = 1, self.used do
-      t[#t + 1] = tostring (a[i])
-    end
-    return Object.prototype (self) .. " (" .. table.concat (t, ", ") .. ")"
   end,
 
 
@@ -148,6 +215,7 @@ local Array = Object {
   -- @function __len
   -- @treturn int number of elements
   __len = function (self)
+    argscheck ("__len", {"Array"}, {self})
     return self.used
   end,
 
@@ -157,7 +225,9 @@ local Array = Object {
   -- @int n 1-based index, or negative to index starting from the right
   -- @treturn string the element at index `n`
   __index = function (self, n)
-    if type (n) == "number" then
+    argscheck ("__index", {"Array", {"number", "string"}}, {self, n})
+
+    if typeof (n) == "number" then
       if n < 0 then n = n + self.used + 1 end
       if n > 0 and n <= self.used then
         return self.array[n]
@@ -174,17 +244,34 @@ local Array = Object {
   -- @param elem value to store at index n
   -- @treturn Array the array
   __newindex = function (self, n, elem)
-    if type (n) == "number" then
-      local a = self.array
-      if n == 0 or math.abs (n) > self.used then
+    argscheck ("__newindex", {"Array", "number", "number"}, {self, n, elem})
+
+    if typeof (n) == "number" then
+      local a, used = self.array, self.used
+      if n == 0 or math.abs (n) > used then
 	return a[0] -- guaranteed to be out of bounds
       end
-      if n < 0 then n = n + self.used + 1 end
+      if n < 0 then n = n + used + 1 end
       a[n] = elem
     else
       rawset (self, n, elem)
     end
     return self
+  end,
+
+
+  --- Return a string representation of the contents of this Array.
+  -- @function __tostring
+  -- @treturn string string representation
+  __tostring = function (self)
+    argscheck ("__tostring", {"Array"}, {self})
+
+    local a = self.array
+    local t = {'"' .. a.type .. '"'}
+    for i = 1, self.used do
+      t[#t + 1] = tostring (a[i])
+    end
+    return prototype (self) .. " (" .. table.concat (t, ", ") .. ")"
   end,
 }
 
